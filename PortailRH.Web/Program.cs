6c50f7a62bd81449cb6ffaafef87ab9d1ec7297d
Mohamed ED-Derkaoui <med.derkaoui99@gmail.com>
2023-08-15 23:04:46 +0100

    Implemented Admin Dashboard: UI components

--- PortailRH.Web/Program.cs
+++ PortailRH.Web/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.RegisterRepositories();
+//builder.Services.RegisterRepositories();
 
 var app = builder.Build();
 
