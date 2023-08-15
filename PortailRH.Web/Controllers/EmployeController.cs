268c12f1f4b69d127603645d039147927121c5ac
Mohamed ED-Derkaoui <med.derkaoui99@gmail.com>
2023-08-15 23:07:26 +0100

    Created Employee controller for backend functionality and commenced design work on the new Employee page

---
+++ PortailRH.Web/Controllers/EmployeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortailRH.Web.Controllers
+{
+    public class EmployeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NouvelEmploye()
+        {
+            return View();
+        }
+    }
+}
